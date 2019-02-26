34c6f79822760c2d94d8d9b25f824132c3aca37d
hzj <424087283@qq.com>
2019-02-26 14:51:07 +0800

    调整部分流程;

--- src/pages/LaborContract/ContractCreate.js
+++ src/pages/LaborContract/ContractCreate.js
@@ -479,7 +479,7 @@ class ContractCreate extends PureComponent {
                   name="file"
                   listType="picture-card"
                   showUploadList={false}
-                  action="http://127.0.0.1:8080/labor/contract/file/add"
+                  action="http://10.80.10.151:8080/labor/contract/file/add"
                   multiple={false}
                   className="uploadBar"
                   onChange={this.handleChange}
--- src/pages/LaborContract/ContractEdit.js
+++ src/pages/LaborContract/ContractEdit.js
@@ -465,7 +465,7 @@ class ContractEdit extends PureComponent {
                   name="file"
                   listType="picture-card"
                   showUploadList={false}
-                  action="http://127.0.0.1:8080/labor/contract/file/add"
+                  action="http://10.80.10.151:8080/labor/contract/file/add"
                   multiple={false}
                   className="uploadBar"
                   onChange={this.handleChange}
